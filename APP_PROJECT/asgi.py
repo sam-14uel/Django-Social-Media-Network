d7f2718932638546e05efe497bbdc74f4b4872b2
Samuel Obinna Chimdi <147158353+sam-14uel@users.noreply.github.com>
2024-10-07 11:38:21 +0100

    added files

---
+++ APP_PROJECT/asgi.py
@@ -0,0 +1,28 @@
+"""
+ASGI config for APP_PROJECT project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from social_network.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'APP_PROJECT.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
